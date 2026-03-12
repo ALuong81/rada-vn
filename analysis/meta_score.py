93bf404f2d2f65aaa40e60e4769a2ce64d970b66
ALuong81 <anhln81@gmail.com>
2026-03-13 03:26:40 +0700

    Update meta_score.py

--- analysis/meta_score.py
+++ analysis/meta_score.py
@@ -1,22 +1,39 @@
-def score_stock(stock):
+def score_stock(s):
 
     score = 0
 
-    volume = stock.get("volume", 0)
-    avg_volume = stock.get("avg_volume", 0)
-    price = stock.get("price", 0)
-    resistance = stock.get("resistance", 0)
+    price = s.get("price",0)
+    resistance = s.get("resistance",0)
+    volume = s.get("volume",0)
+    avg_volume = s.get("avg_volume",0)
+    change = s.get("change",0)
 
-    # breakout strength
-    if price > resistance * 0.97:
-        score += 40
+    # breakout proximity
+    if resistance > 0:
+        dist = price / resistance
+
+        if dist > 1:
+            score += 60
+        elif dist > 0.98:
+            score += 40
+        elif dist > 0.95:
+            score += 20
 
     # volume expansion
-    if avg_volume > 0 and volume > avg_volume * 1.5:
-        score += 40
+    if avg_volume > 0:
+        vol_ratio = volume / avg_volume
 
-    # supply dry-up
-    if avg_volume > 0 and volume < avg_volume * 0.7:
-        score += 30
+        if vol_ratio > 2:
+            score += 40
+        elif vol_ratio > 1.5:
+            score += 30
+        elif vol_ratio > 1.2:
+            score += 20
+
+    # momentum
+    if change > 3:
+        score += 20
+    elif change > 1:
+        score += 10
 
     return score

ab36ed924ba9c4bfe28c6cbe8735b1c1519f3717
ALuong81 <anhln81@gmail.com>
2026-03-13 03:05:40 +0700

    Update meta_score.py

--- analysis/meta_score.py
+++ analysis/meta_score.py
@@ -1,14 +1,22 @@
-def meta_score(stock):
+def score_stock(stock):
 
-    score=0
+    score = 0
 
-    if stock["volume"]>stock["avg_volume"]*1.5:
-        score+=40
+    volume = stock.get("volume", 0)
+    avg_volume = stock.get("avg_volume", 0)
+    price = stock.get("price", 0)
+    resistance = stock.get("resistance", 0)
 
-    if stock["price"]>stock["resistance"]*0.95:
-        score+=40
+    # breakout strength
+    if price > resistance * 0.97:
+        score += 40
 
-    if stock["volume"]<stock["avg_volume"]*0.7:
-        score+=30
+    # volume expansion
+    if avg_volume > 0 and volume > avg_volume * 1.5:
+        score += 40
+
+    # supply dry-up
+    if avg_volume > 0 and volume < avg_volume * 0.7:
+        score += 30
 
     return score

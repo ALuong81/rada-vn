2283aeb4e68dc16c2edc3be635c530a067cac61f
ALuong81 <anhln81@gmail.com>
2026-03-13 06:04:34 +0700

    Update liquidity_filter.py

--- analysis/liquidity_filter.py
+++ analysis/liquidity_filter.py
@@ -1,7 +1,15 @@
-def liquidity_filter(stock):
+def liquidity_ok(stock):
 
-    if stock["avg_volume"] < 100000:
+    vol = stock.get("volume",0)
+    avg = stock.get("avg_volume",0)
 
+    if avg == 0:
+        return False
+
+    if avg < 100000:
+        return False
+
+    if vol < avg * 0.5:
         return False
 
     return True

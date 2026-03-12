7566459c4d6ab075c7fda6b742ac594b52a8f207
ALuong81 <anhln81@gmail.com>
2026-03-13 01:57:54 +0700

    Update breakout_engine.py

--- analysis/breakout_engine.py
+++ analysis/breakout_engine.py
@@ -1,10 +1,10 @@
 def breakout(stock):
 
-    if stock["price"] > stock["resistance"] * 0.97:
+    if stock["price"] > stock["resistance"]:
 
         return "VƯỢT ĐỈNH","XÁC SUẤT CAO"
 
-    if stock["price"] > stock["resistance"]*0.98:
+    if stock["price"] > stock["resistance"]*0.95:
 
         return "Chuẩn bị breakout","XÁC SUẤT CAO"
 

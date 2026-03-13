22dbe9fc6f301eefdc38e3e0e71c7996b515cb37
ALuong81 <anhln81@gmail.com>
2026-03-13 13:48:26 +0700

    Update vcp_detector.py

--- analysis/vcp_detector.py
+++ analysis/vcp_detector.py
@@ -1,25 +1,36 @@
-def vcp_pattern(stocks):
+def detect_vcp(stock):
+
+    price = stock.get("price", 0)
+    resistance = stock.get("resistance", 0)
+    volume = stock.get("volume", 0)
+    avg_volume = stock.get("avg_volume", 0)
+
+    if resistance == 0:
+        stock["vcp"] = False
+        return stock
+
+    tight_range = price > resistance * 0.85
+    volume_dry = volume < avg_volume * 0.8
+
+    if tight_range and volume_dry:
+        stock["vcp"] = True
+    else:
+        stock["vcp"] = False
+
+    return stock
+
+
+def scan_vcp(stocks):
+
+    results = []
 
     for s in stocks:
 
-        price = s.get("price", 0)
-        resistance = s.get("resistance", 0)
-        volume = s.get("volume", 0)
-        avg_volume = s.get("avg_volume", 0)
-
-        if resistance == 0:
-            s["vcp"] = "KHÔNG"
+        if not isinstance(s, dict):
             continue
 
-        # giá đang co gần kháng cự
-        tight_price = price > resistance * 0.85
+        s = detect_vcp(s)
 
-        # volume đang giảm
-        dry_volume = volume < avg_volume * 0.8
+        results.append(s)
 
-        if tight_price and dry_volume:
-            s["vcp"] = "CÓ"
-        else:
-            s["vcp"] = "KHÔNG"
-
-    return stocks
+    return results

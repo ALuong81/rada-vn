a77d71b7dccfd9a02612ac3c0e5dc3dba3064ffc
ALuong81 <anhln81@gmail.com>
2026-03-13 19:52:27 +0700

    Create sector_map.py

---
+++ analysis/sector_map.py
@@ -0,0 +1,59 @@
+SECTOR_MAP = {
+
+# NGÂN HÀNG
+"VCB":"NGÂN HÀNG","BID":"NGÂN HÀNG","CTG":"NGÂN HÀNG","TCB":"NGÂN HÀNG",
+"MBB":"NGÂN HÀNG","ACB":"NGÂN HÀNG","STB":"NGÂN HÀNG","VPB":"NGÂN HÀNG",
+"HDB":"NGÂN HÀNG","TPB":"NGÂN HÀNG","EIB":"NGÂN HÀNG","MSB":"NGÂN HÀNG",
+"SHB":"NGÂN HÀNG","LPB":"NGÂN HÀNG","VIB":"NGÂN HÀNG","BAB":"NGÂN HÀNG",
+
+# CHỨNG KHOÁN
+"SSI":"CHỨNG KHOÁN","VND":"CHỨNG KHOÁN","HCM":"CHỨNG KHOÁN",
+"SHS":"CHỨNG KHOÁN","VCI":"CHỨNG KHOÁN","MBS":"CHỨNG KHOÁN",
+"FTS":"CHỨNG KHOÁN","BVS":"CHỨNG KHOÁN","AGR":"CHỨNG KHOÁN",
+
+# BẤT ĐỘNG SẢN
+"VHM":"BẤT ĐỘNG SẢN","NVL":"BẤT ĐỘNG SẢN","PDR":"BẤT ĐỘNG SẢN",
+"KDH":"BẤT ĐỘNG SẢN","DXG":"BẤT ĐỘNG SẢN","DIG":"BẤT ĐỘNG SẢN",
+"CEO":"BẤT ĐỘNG SẢN","HDC":"BẤT ĐỘNG SẢN","NLG":"BẤT ĐỘNG SẢN",
+"SCR":"BẤT ĐỘNG SẢN","HDG":"BẤT ĐỘNG SẢN","IDC":"BẤT ĐỘNG SẢN",
+
+# XÂY DỰNG
+"VCG":"XÂY DỰNG","CTD":"XÂY DỰNG","HBC":"XÂY DỰNG",
+"HHV":"XÂY DỰNG","LCG":"XÂY DỰNG","C4G":"XÂY DỰNG",
+
+# DẦU KHÍ
+"GAS":"DẦU KHÍ","PLX":"DẦU KHÍ","BSR":"DẦU KHÍ",
+"PVD":"DẦU KHÍ","PVS":"DẦU KHÍ","OIL":"DẦU KHÍ",
+
+# ĐIỆN – NĂNG LƯỢNG
+"POW":"ĐIỆN","NT2":"ĐIỆN","REE":"ĐIỆN",
+"GEG":"ĐIỆN","PC1":"ĐIỆN","HDG":"ĐIỆN",
+
+# PHÂN BÓN – HÓA CHẤT
+"DPM":"PHÂN BÓN","DCM":"PHÂN BÓN","LAS":"PHÂN BÓN",
+"DGC":"HÓA CHẤT","CSV":"HÓA CHẤT",
+
+# THÉP
+"HPG":"THÉP","HSG":"THÉP","NKG":"THÉP",
+
+# BÁN LẺ
+"MWG":"BÁN LẺ","FRT":"BÁN LẺ","DGW":"BÁN LẺ","PNJ":"BÁN LẺ",
+
+# HÀNG KHÔNG
+"HVN":"HÀNG KHÔNG","VJC":"HÀNG KHÔNG",
+
+# CẢNG – LOGISTICS
+"GMD":"LOGISTICS","HAH":"LOGISTICS","VSC":"LOGISTICS",
+
+# THỦY SẢN
+"VHC":"THỦY SẢN","ANV":"THỦY SẢN","IDI":"THỦY SẢN",
+
+# DỆT MAY
+"TCM":"DỆT MAY","MSH":"DỆT MAY","STK":"DỆT MAY",
+
+# CÔNG NGHỆ
+"FPT":"CÔNG NGHỆ","CMG":"CÔNG NGHỆ",
+
+# TÀI CHÍNH
+"EVF":"TÀI CHÍNH","VIX":"TÀI CHÍNH"
+}

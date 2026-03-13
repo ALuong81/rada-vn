e1d265427e214b9888edd3c131f277b05dba1720
ALuong81 <anhln81@gmail.com>
2026-03-13 12:45:56 +0700

    Update fake_breakout_filter.py

--- analysis/fake_breakout_filter.py
+++ analysis/fake_breakout_filter.py
@@ -1,7 +1,22 @@
-def fake_breakout_filter(stock):
+def filter_fake_breakout(stocks):
 
-    if stock["volume"] < stock["avg_volume"] * 0.7:
+    filtered = []
 
-        return True
+    for s in stocks:
 
-    return False
+        price = s.get("price", 0)
+        resistance = s.get("resistance", 0)
+        volume = s.get("volume", 0)
+        avg = s.get("avg_volume", 0)
+
+        if resistance == 0:
+            continue
+
+        breakout = price > resistance * 0.98
+
+        vol_ok = volume > avg * 1.2
+
+        if breakout and vol_ok:
+            filtered.append(s)
+
+    return filtered

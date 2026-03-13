6196b64a5e8d85591d68e55daa571e15f445aae0
ALuong81 <anhln81@gmail.com>
2026-03-14 03:28:38 +0700

    Create vnindex_data_engine.py

---
+++ analysis/vnindex_data_engine.py
@@ -0,0 +1,14 @@
+def get_vnindex_data():
+
+    # demo data
+    # sau có thể thay bằng API thật
+
+    data = [
+        {"close": 1240, "volume": 800000},
+        {"close": 1235, "volume": 900000},
+        {"close": 1238, "volume": 850000},
+        {"close": 1245, "volume": 920000},
+        {"close": 1250, "volume": 1000000},
+    ]
+
+    return data

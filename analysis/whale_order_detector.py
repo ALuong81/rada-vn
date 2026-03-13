14e09cbe971717fc8f4b028d7efb9c0b9939dd4d
ALuong81 <anhln81@gmail.com>
2026-03-14 02:59:36 +0700

    Create whale_order_detector.py

---
+++ analysis/whale_order_detector.py
@@ -0,0 +1,15 @@
+def detect_whale_orders(s):
+
+    volume = s.get("volume",0)
+    avg_volume = s.get("avg_volume",0)
+
+    if avg_volume == 0:
+        return False
+
+    if volume > avg_volume * 3:
+        return "CÁ VOI HOẠT ĐỘNG"
+
+    if volume > avg_volume * 2:
+        return "DÒNG TIỀN LỚN"
+
+    return "BÌNH THƯỜNG"

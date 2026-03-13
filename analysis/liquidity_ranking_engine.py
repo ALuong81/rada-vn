c5a526af5d358ad80c531344b073cc57f83e8c4a
ALuong81 <anhln81@gmail.com>
2026-03-13 23:54:17 +0700

    Create liquidity_ranking_engine.py

---
+++ analysis/liquidity_ranking_engine.py
@@ -0,0 +1,15 @@
+def liquidity_ranking(stocks):
+
+    filtered = []
+
+    for s in stocks:
+
+        volume = s.get("volume",0)
+        avg_volume = s.get("avg_volume",0)
+
+        if avg_volume > 200000:
+            filtered.append(s)
+
+    filtered = sorted(filtered, key=lambda x: x["avg_volume"], reverse=True)
+
+    return filtered[:300]

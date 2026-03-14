0df18ea2289eef008536c04007f726f30337cc80
ALuong81 <anhln81@gmail.com>
2026-03-14 20:57:31 +0700

    Update main_engine.py

--- engine/main_engine.py
+++ engine/main_engine.py
@@ -129,7 +129,7 @@ def run():
     stocks = scan_smart_money(stocks)
     stocks = scan_volume(stocks)
     stocks = supply_dryup(stocks)
-    stocks = scan_risk(stocks)l
+    stocks = scan_risk(stocks)
 
     stocks = detect_leaders(stocks)
     stocks = filter_fake_breakout(stocks)

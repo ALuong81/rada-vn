701428422c5e01b647bbf0d27cb1f0643038666b
ALuong81 <anhln81@gmail.com>
2026-03-14 21:03:30 +0700

    Update supply_dryup_detector.py

--- analysis/supply_dryup_detector.py
+++ analysis/supply_dryup_detector.py
@@ -1,13 +1,17 @@
 def supply_dryup(stock):
 
-    ratio = stock["volume"] / stock["avg_volume"]
+    vol = stock.get("volume",0)
+    avg = stock.get("avg_volume",0)
+
+    if avg == 0:
+        return "BÌNH THƯỜNG"
+
+    ratio = vol / avg
 
     if ratio < 0.6:
-
         return "CUNG CẠN"
 
     if ratio < 0.8:
-
         return "SIẾT CUNG"
 
     return "BÌNH THƯỜNG"

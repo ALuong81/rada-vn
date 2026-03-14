8795fc3bac032fd6c936d99d935de156a645a633
ALuong81 <anhln81@gmail.com>
2026-03-14 19:49:50 +0700

    Update vnindex_trend_engine.py

--- analysis/vnindex_trend_engine.py
+++ analysis/vnindex_trend_engine.py
@@ -1,13 +1,24 @@
-def vnindex_trend(index):
+def vnindex_trend(index_data):
 
-    close = index.get("close",0)
-    ma50 = index.get("ma50",0)
-    ma200 = index.get("ma200",0)
+    """
+    Xác định xu hướng VNINDEX
+    """
 
-    if close > ma50 and ma50 > ma200:
+    if not index_data or len(index_data) < 50:
+        return "UNKNOWN"
+
+    closes = [d.get("close", 0) for d in index_data]
+
+    ma20 = sum(closes[-20:]) / 20
+    ma50 = sum(closes[-50:]) / 50
+    ma200 = sum(closes[-200:]) / 200
+    
+    last_close = closes[-1]
+
+    if last_close > ma50 and ma50 > ma200:
         return "UPTREND"
 
-    if close < ma50 and ma50 < ma200:
+    if last_close < ma200 and ma20 < ma50:
         return "DOWNTREND"
 
     return "SIDEWAY"

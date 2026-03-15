ac7b3e7bf7a089983853ae20c0bf119295599248
ALuong81 <anhln81@gmail.com>
2026-03-15 08:07:15 +0700

    Update liquidity_ranking_engine.py

--- analysis/liquidity_ranking_engine.py
+++ analysis/liquidity_ranking_engine.py
@@ -7,7 +7,7 @@ def liquidity_ranking(stocks):
         volume = s.get("volume",0)
         avg_volume = s.get("avg_volume",0)
 
-        if avg_volume > 200000:
+        if avg_volume > 500000:
             filtered.append(s)
 
     filtered = sorted(filtered, key=lambda x: x["avg_volume"], reverse=True)
